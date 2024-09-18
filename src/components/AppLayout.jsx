0488547e8f91263c02c9bccc3fbebb5cdea138b0
williams <williamsgoddey11@gmail.com>
2024-09-18 12:08:59 +0100

    reset

--- src/components/AppLayout.jsx
+++ src/components/AppLayout.jsx
@@ -1,11 +1,12 @@
 import React, { useContext, useState } from 'react'
-import UserAvatar from './userAvatar';
+
 import Form from './Form';
 import Weather from './Weather';
 import { WeatherContext } from '../WeatherProvider';
 import CurrentWeather from './CurrentWeather';
 import TemperatureChart from './TemperatureChart';
 import LoadingSpinner from './LoadingSpinner';
+import Avatar from './Avatar';
 
 export default function AppLayout() {
     const [loading, setLoading] = useState(false);
@@ -16,7 +17,7 @@ export default function AppLayout() {
       <div className="md:grid md:grid-cols-[13rem,30rem] items-center lg:grid-cols-[13.5rem,35.6rem] md:bg-white  md:rounded-lg md:shadow-md lg:w-[830px] overflow-hidden md:gap-x-3 md:p-3 lg:p-6">
         <div className="bg-white rounded-[2rem] md:rounded-none p-2">
           <div className="justify-end flex md:hidden mb-5  p-3">
-            <UserAvatar />
+            <Avatar />
           </div>
           <Form setLoading={setLoading} cityName={setCityName} />
           {loading && <LoadingSpinner/>}
@@ -27,7 +28,7 @@ export default function AppLayout() {
 
         <div>
           <div className='md:flex hidden  justify-end mb-5'>
-          <UserAvatar />
+          <Avatar />
           </div>
           <TemperatureChart />
           {weatherData.weathercode && <Weather />}
---
+++ src/components/Avatar.jsx
@@ -0,0 +1,36 @@
+import React, { useState } from "react";
+import { FaAngleDown, FaAngleUp, FaCircleUser } from "react-icons/fa6";
+import Modal from "./Modal";
+import useLogout from "./useLogout";
+import useUser from "./useUser";
+import MiniLoader from "./miniLoader";
+
+export default function Avatar() {
+  const { data, isPending } = useUser();
+
+  const { user_metadata } = data;
+  const { mutate } = useLogout();
+  const [open, setOpen] = useState(false);
+  const handleClick = function () {
+    setOpen(!open);
+  };
+  return (
+    <div className="flex relative items-center  gap-x-2">
+      <FaCircleUser />
+      <div className="flex gap-x-2 items-center ">
+        <span className="font-medium capitalize">
+          {isPending ? <MiniLoader /> : user_metadata.name}
+        </span>
+        <FaAngleDown
+          className="text-stone-400 cursor-pointer"
+          onClick={handleClick}
+        />
+      </div>
+      {open && (
+        <div className="absolute top-5">
+          <Modal />
+        </div>
+      )}
+    </div>
+  );
+}
